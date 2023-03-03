e5a592ce73531ae8e61a4af4685806c5e122fc50
rain8128 <126866571+rain8128@users.noreply.github.com>
2023-03-03 19:14:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ window.onload = (d)=>{
   d = document;
   let btn = d.getElementById("create");
   btn.onclick = ()=>{
-    let handle = d.getElementById("name").value;
-    let email = d.getElementById("email").value;
-    let password = d.getElementById("password").value;
+    let handle = '"' + d.getElementById("name").value + '"';
+    let email = '"' + d.getElementById("email").value + '"';
+    let password = '"' + d.getElementById("password").value + '"';
     let inviteCode = d.getElementById("invite-code").value;
-    let m = '{"handle":' + handle + ',"email":' + email + ',"password":' + password + (inviteCode ? (',"inviteCode":' + inviteCode) : "") + '}';
+    let m = '{"handle":' + handle + ',"email":' + email + ',"password":' + password + (inviteCode ? (',"inviteCode":' + '"' + inviteCode + '"') : "") + '}';
     let xhr = new XMLHttpRequest();
     xhr.open("POST","https://bsky.social/xrpc/com.atproto.account.create");
     xhr.setRequestHeader("Content-Type","application/json");
