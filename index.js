e031b549bdf30931398414166717d29e73a76537
rain8128 <126866571+rain8128@users.noreply.github.com>
2023-03-04 17:04:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ window.onload = (d)=>{
   d = document;
   let btn = d.getElementById("create");
   btn.onclick = ()=>{
-    let handle = '"' + d.getElementById("name").value + '"';
-    let email = '"' + d.getElementById("email").value + '"';
-    let password = '"' + d.getElementById("password").value + '"';
+    let handle = d.getElementById("name").value;
+    let email = d.getElementById("email").value;
+    let password = d.getElementById("password").value;
     let inviteCode = d.getElementById("invite-code").value;
-    let m = '{"handle":' + handle + ',"email":' + email + ',"password":' + password + (inviteCode ? (',"inviteCode":' + '"' + inviteCode + '"') : "") + '}';
+    let m = {"handle": handle,
+             "email": email,
+             "password": password}
+    if(inviteCode){
+      m.inviteCode = inviteCode;
+    }
     let xhr = new XMLHttpRequest();
     xhr.open("POST","https://bsky.social/xrpc/com.atproto.account.create");
     xhr.setRequestHeader("Content-Type","application/json");
-    xhr.send(m);
+    xhr.send(JSON.stringify(m));
+    xhr.onreadychange = () => {
+      if(xhr.readystate == XMLHttpReqest.DONE){
+        d.getElementById("respMessage").innerText = xhr.responsText
+    }
   };
 }
